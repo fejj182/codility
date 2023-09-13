ca4e5287cb44097f165338c7d922405551619f89
Jeff <jzinger100@gmail.com>
2023-09-13 21:33:22 +0200

    lesson_6 understand merge sort

---
+++ lesson_6/merge_sort.js
@@ -0,0 +1,46 @@
+function mergeSort(array) {
+    if (array.length === 1) {
+        return array
+    }
+
+    let left = []
+    let right = []
+
+    for (let i=0; i < array.length; i++) {
+        if (i < (array.length / 2)) {
+            left.push(array[i])
+        } else {
+            right.push(array[i])
+        }
+    }
+
+    left = mergeSort(left)
+    right = mergeSort(right)
+    
+    return merge(left, right)
+}
+
+function merge(left, right) {
+    const result = []
+    while (left.length > 0 && right.length > 0) {
+        if (left[0] < right[0]) {
+            const firstLeft = left.shift()
+            result.push(firstLeft)
+        } else {
+            const firstRight = right.shift()
+            result.push(firstRight)
+        }
+    }
+
+    while (left.length > 0) {
+        const firstLeft = left.shift() 
+        result.push(firstLeft)
+    } 
+    while (right.length > 0) {
+        const firstRight = right.shift() 
+        result.push(firstRight)
+    }
+    return result
+}
+
+module.exports = mergeSort
---
+++ lesson_6/merge_sort.test.js
@@ -0,0 +1,49 @@
+const mergeSort = require("./merge_sort");
+
+//NOTE: you iterate through an array and create a count of all the occurrences of each possible number.
+//To know each possible number you must know the range.
+//You then iterate through the array of counts and recreate the array in the right order,
+//considering the number of times they were originally counted.
+
+describe('count sort', () => {
+    it('should return the same array if length is 1', () => {
+        expect(mergeSort([1])).toEqual([1])
+    });
+
+    it('should return the same array in order', () => {
+        expect(mergeSort([0,1])).toEqual([0,1])
+    });
+
+    it('should return the same array missing 1', () => {
+        expect(mergeSort([0,2])).toEqual([0,2])
+    });
+
+    it('should return the same array with two of same', () => {
+        expect(mergeSort([0,0,2])).toEqual([0,0,2])
+    });
+
+    it('should return the same array with higher range', () => {
+        expect(mergeSort([0,1,2])).toEqual([0,1,2])
+    });
+
+    it('should return the reversed array', () => {
+        expect(mergeSort([1,0])).toEqual([0,1])
+    });
+
+    it('should return the reversed array with repeat', () => {
+        expect(mergeSort([1,0,0])).toEqual([0,0,1])
+    });
+
+
+    it('test case', () => {
+        expect(mergeSort([3,1,2,0])).toEqual([0,1,2,3])
+    });
+
+    it('test case long', () => {
+        expect(mergeSort([6,5,3,1,8,7,2,4])).toEqual([1,2,3,4,5,6,7,8])
+    });
+
+    it('test case long 2', () => {
+        expect(mergeSort([6,2,3,5,8,7,1,4])).toEqual([1,2,3,4,5,6,7,8])
+    });
+});
